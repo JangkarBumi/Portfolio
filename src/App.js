3a764e142e1e4ed113f036a2937324ef5b643a15
Zeha Irawan <jangkaringbumi@gmail.com>
2019-08-26 17:52:52 +0700

    Add navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-   <h1>My Project</h1>
+      <h1>My Project</h1>
+      <Navbar></Navbar>
     </div>
   );
 }
