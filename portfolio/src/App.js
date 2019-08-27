c9c5be8f11a576296c97ad3a16ac59ed48cc495f
Zeha Irawan <jangkaringbumi@gmail.com>
2019-08-27 23:19:06 +0700

    update components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Contact from './components/Contact';
 import Navbar from './components/Navbar';
+import Project from './components/Project';
 
 function App() {
   return (
     <div className="App">
       <h1>My Project</h1>
       <Navbar></Navbar>
+      <Project></Project>
+      <Contact></Contact>
     </div>
   );
 }
