cc63ca8a63002d5bf8bde015e61dfde4b3e3c6d4
Zeha Irawan <jangkaringbumi@gmail.com>
2019-09-05 20:12:38 +0700

    Add hidden input

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -28,6 +28,7 @@ const Contact = () => {
         </li>
       </ul>
       <form name="contact" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>
             Your Name: <input type="text" name="name" />
